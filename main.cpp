cd056a57dcd2b157422c5da86be869cb495b83dc
User <sevaserg@gmail.com>
2019-04-20 23:08:17 +0300

    Программа завершена, добавлен сейв-файл с сэмплами. Осталось разделить на файлы.

--- main.cpp
+++ main.cpp
@@ -8,7 +8,6 @@
 */
 
 #include <windows.h>
-
 #include <iostream>
 #include <fstream>
 #include <cstdio>
@@ -16,14 +15,15 @@
 
 using namespace std;
 
-string countrieslist[195];
+
 
 char charbuf[100];
 
-string materials[5] = {"plain paper","foil paper","copper","silver","non-listed material"};
-string shapes[5] = {"square","triangle","round","rectangle","other"};
+string countrieslist[195];
 string purposes[3] = {"common purpose","special purpose","non-postal use"};
 string emittypes[4] = {"official","semi-official","non-official","private post"};
+string materials[5] = {"plain paper","foil paper","copper","silver","non-listed material"};
+string shapes[5] = {"square","triangle","round","rectangle","other"};
 
 struct mark
 {
@@ -33,223 +33,344 @@ struct mark
     char emittype;
     char shape;
     char material;
-    string amount;
     string prestory;
 };
 
 class commonfactory
 {
-protected:
-    mark* m;
-    int amt;
-public:
-    virtual void prestory(mark &k) = 0;
-    void setmark(string country)
-    {
-        mark* buf = new mark[amt];
-        for (int i = 0; i < amt; i++) buf[i] = m[i];
-        amt=amt+1;
-        mark* m = new mark[amt];
-        for (int i = 0; i < amt-1; i++) m[i] = buf[i];
-        m[amt-1].country = country;
-        delete[]buf;
-        cout<<"Name: ";
-        scanf(" %[^\n]s",charbuf);
-        string buf2(charbuf);
-        m[amt-1].name = buf2;
-        cout<<"Purpose: "<<endl;
-        for (int i = 0; i < 3; i++) cout<<i+1<<" - "<<purposes[i]<<endl;
-        do{
-            cin>>m[amt-1].purpose;
-            if (m[amt-1].purpose<'1'||m[amt-1].purpose>'3')
-                cout<<"Input error! A single number (1-3) was requested."<<endl;
-        }while(m[amt-1].purpose<'1'||m[amt-1].purpose>'3');
-        cout<<"Emitient type: "<<endl;
-        for (int i = 0; i < 4; i++) cout<<i+1<<" - "<<emittypes[i]<<endl;
-        do{
-            cin>>m[amt-1].emittype;
-            if (m[amt-1].emittype<'1'||m[amt-1].emittype>'4')
-                cout<<"Input error! A single number (1-4) was requested."<<endl;
-        }while(m[amt-1].emittype<'1'||m[amt-1].emittype>'4');
-        cout<<"Shape: "<<endl;
-        for (int i = 0; i < 5; i++) cout<<i+1<<" - "<<shapes[i]<<endl;
-        do{
-            cin>>m[amt-1].shape;
-            if (m[amt-1].shape<'1'||m[amt-1].shape>'5')
-                cout<<"Input error! A single number (1-5) was requested."<<endl;
-        }while(m[amt-1].shape<'1'||m[amt-1].shape>'5');
-        cout<<"Material: "<<endl;
-        for (int i = 0; i < 5; i++) cout<<i+1<<" - "<<materials[i]<<endl;
-        do{
-            cin>>m[amt-1].shape;
-            if (m[amt-1].shape<'1'||m[amt-1].shape>'5')
-                cout<<"Input error! A single number (1-5) was requested."<<endl;
-        }while(m[amt-1].shape<'1'||m[amt-1].shape>'5');
-        prestory(m[amt-1]);
-    }
-
-    int getamt()
-    {
-        return amt;
-    }
-
-    mark getmark(int num)
-    {
-        return m[num];
-    }
-
-    void deletemark(int mk)
-    {
-        amt--;
-        int j = 0;
-        mark* buf = new mark[amt];
-        for (int i = 0; i < amt; i++)
+    protected:
+        mark* m;
+        int amt;
+    public:
+        void deleteall()
         {
-            if (j == mk) j++;
-            buf[i] = m[j];
-            j++;
+            amt = 0;
         }
-        m = new mark[amt];
-        for (int i = 0; i < amt; i++) m[i] = buf[i];
-        delete[]buf;
-    }
+        virtual void prestory(mark& mk) = 0;
+        void setmark(string country)
+        {
+                amt++;
+                if (amt == 1)
+                {
+                    m = new mark;
+                    //-------------------
+                    m->country = country;
+                    cout<<"Name: ";
+                    scanf(" %[^\n]s",charbuf);
+                    string buf2(charbuf);
+                    m->name = buf2;
+                    cout<<"Purpose: "<<endl;
+                    for (int i = 0; i < 3; i++) cout<<i+1<<" - "<<purposes[i]<<endl;
+                    do{
+                        cin>>m->purpose;
+                        if (m->purpose<'1'||m->purpose>'3')
+                            cout<<"Input error! A single number (1-3) was requested."<<endl;
+                    }while(m->purpose<'1'||m->purpose>'3');
+                    cout<<"Emitient type: "<<endl;
+                    for (int i = 0; i < 4; i++) cout<<i+1<<" - "<<emittypes[i]<<endl;
+                    do{
+                        cin>>m->emittype;
+                        if (m->emittype<'1'||m->emittype>'4')
+                            cout<<"Input error! A single number (1-4) was requested."<<endl;
+                    }while(m->emittype<'1'||m->emittype>'4');
+                    cout<<"Shape: "<<endl;
+                    for (int i = 0; i < 5; i++) cout<<i+1<<" - "<<shapes[i]<<endl;
+                    do{
+                        cin>>m->shape;
+                        if (m->shape<'1'||m->shape>'5')
+                            cout<<"Input error! A single number (1-5) was requested."<<endl;
+                    }while(m->shape<'1'||m->shape>'5');
+                    cout<<"Material: "<<endl;
+                    for (int i = 0; i < 5; i++) cout<<i+1<<" - "<<materials[i]<<endl;
+                    do{
+                        cin>>m->material;
+                        if (m->material<'1'||m->material>'5')
+                            cout<<"Input error! A single number (1-5) was requested."<<endl;
+                    }while(m->material<'1'||m->material>'5');
+                    prestory(*m);
+                }
+                else
+                {
+                    mark* buf = new mark[amt-1];
+                    for (int i = 0; i < amt-1; ++i) buf[i] = m[i];
+                    m = new mark[amt];
+                    for (int i = 0; i < amt-1; ++i) m[i] = buf[i];
+                    //------------------------
+                    m[amt-1].country = country;
+                    cout<<"Name: ";
+                    scanf(" %[^\n]s",charbuf);
+                    string buf2(charbuf);
+                    m[amt-1].name = buf2;
+                    cout<<"Purpose: "<<endl;
+                    for (int i = 0; i < 3; i++) cout<<i+1<<" - "<<purposes[i]<<endl;
+                    do{
+                        cin>>m[amt-1].purpose;
+                        if (m[amt-1].purpose<'1'||m[amt-1].purpose>'3')
+                            cout<<"Input error! A single number (1-3) was requested."<<endl;
+                    }while(m[amt-1].purpose<'1'||m[amt-1].purpose>'3');
+                    cout<<"Emitient type: "<<endl;
+                    for (int i = 0; i < 4; i++) cout<<i+1<<" - "<<emittypes[i]<<endl;
+                    do{
+                        cin>>m[amt-1].emittype;
+                        if (m[amt-1].emittype<'1'||m[amt-1].emittype>'4')
+                            cout<<"Input error! A single number (1-4) was requested."<<endl;
+                    }while(m[amt-1].emittype<'1'||m[amt-1].emittype>'4');
+                    cout<<"Shape: "<<endl;
+                    for (int i = 0; i < 5; i++) cout<<i+1<<" - "<<shapes[i]<<endl;
+                    do{
+                        cin>>m[amt-1].shape;
+                        if (m[amt-1].shape<'1'||m[amt-1].shape>'5')
+                            cout<<"Input error! A single number (1-5) was requested."<<endl;
+                    }while(m[amt-1].shape<'1'||m[amt-1].shape>'5');
+                    cout<<"Material: "<<endl;
+                    for (int i = 0; i < 5; i++) cout<<i+1<<" - "<<materials[i]<<endl;
+                    do{
+                        cin>>m[amt-1].material;
+                        if (m[amt-1].material<'1'||m[amt-1].material>'5')
+                            cout<<"Input error! A single number (1-5) was requested."<<endl;
+                    }while(m[amt-1].material<'1'||m[amt-1].material>'5');
+                    prestory(m[amt-1]);
+                }
+        }
 
-    void show(int start)
-    {
+        void getfromfile(mark mk)
+        {
+            amt++;
+                if (amt == 1)
+                {
+                    m = new mark;
+                    //-------------------
+                    *m = mk;
+                }
+                else
+                {
+                    mark* buf = new mark[amt-1];
+                    for (int i = 0; i < amt-1; ++i) buf[i] = m[i];
+                    m = new mark[amt];
+                    for (int i = 0; i < amt-1; ++i) m[i] = buf[i];
+                    //------------------------
+                    m[amt-1] = mk;
+                }
+        }
 
-        mark temp;
-        if (amt>0)
-            for (int i = 0; i < amt; i++)
+        int getamt()
+        {
+            return amt;
+        }
+
+        mark getmark(int num)
+        {
+            return m[num];
+        }
+
+        void deletemark(int mk)
+        {
+            amt--;
+            if (amt > 0)
             {
-                temp = m[i];
-                cout<<"1";
-                cout<<i+start<<": "<<temp.name<<", "<<temp.country<< ", "<<temp.amount << ((temp.amount == "1") ? " piece. Made of " : " pcs. Made of ") << materials[temp.material-'0'-1]
-                <<", for "<<purposes[temp.purpose-'0'-1]<<"."<<endl<<"Emitent type: "<<emittypes[temp.emittype-'0'-1]<<". Shape: "<<shapes[temp.shape-'0'-1]<<". "
-                <<((temp.prestory == "---")?temp.prestory: "")<<endl<<"----------------------------------------------"<<endl;
+                if (amt>1)
+                {
+                    mark* buf = new mark[amt];
+                    int j = 0;
+                    for (int i = 0; i < amt; ++i)
+                    {
+                        if (i == mk) j++;
+                        buf[i] = m[j];
+                        j++;
+                    }
+                    m = new mark[amt];
+                    for (int i = 0;i<amt;++i) m[i] = buf[i];
+                    
+                }
+                else
+                {
+                    mark buf = m[1-mk];
+                    m = new mark;
+                    *m = buf;
+                }
             }
-    }
+        }
+
+        void show(int start)
+        {
+            mark temp;
+            if (amt>0)
+                for (int i = 0; i < amt; i++)
+                {
+                    temp = m[i];
+                    cout<<i+start<<": "<<temp.name<<", "<<temp.country<< ", made of " << materials[temp.material-'0'-1]
+                    <<", for "<<purposes[temp.purpose-'0'-1]<<"."<<endl<<"Emitent type: "<<emittypes[temp.emittype-'0'-1]<<". Shape: "<<shapes[temp.shape-'0'-1]<<". "
+                    <<((temp.prestory != "---")?temp.prestory: "")<<endl<<"----------------------------------------------"<<endl;
+                }
+        }
+
 };
 
 class realmark : public commonfactory
 {
-public:
+    public:
 
-    realmark()
-    {
-        amt = 0;
-    }
-    void prestory(mark& mk)
-    {
-        cout<<"Pre-story(if not needed, type three '-' symbols): ";
-        scanf(" %[^\n]s",charbuf);
-        string buf3(charbuf);
-        mk.prestory=buf3;
-    }
-    ~realmark()
-    {
-        delete[]m;
-    }
+        realmark()
+        {
+            amt = 0;
+        }
+        void prestory(mark& mk)override
+        {
+            cout<<"Pre-story(if not needed, type three '-' symbols): ";
+            scanf(" %[^\n]s",charbuf);
+            string buf3(charbuf);
+            mk.prestory=buf3;
+        }
+        ~realmark()
+        {
+            delete[]m;
+        }
 };
 
 class virmark : public commonfactory
 {
-public:
-    virmark()
-    {
-        amt = 0;
-    }
-    void prestory(mark& mk)
-    {
-        mk.prestory = "---";
-    }
-    ~virmark()
-    {
-        delete[]m;
-    }
+    public:
+        virmark()
+        {
+            amt = 0;
+        }
+        void prestory(mark& mk) override
+        {
+            mk.prestory = "---";
+        }
+        ~virmark()
+        {
+            delete[]m;
+        }
 };
 
 class mainfactory
 {
-private:
-    virmark* vm;
-    realmark* rm;
-    string* buf;
-public:
-    mainfactory()
-    {
-        rm = new realmark;
-        vm = new virmark;
-    }
-
-    void show()
-    {
-        /*mark temp;
-        if (rm->getamt()>0)
-        for (int i = 0; i < rm->getamt();i++)
+    private:
+        virmark* vm;
+        realmark* rm;
+        string buf;
+    public:
+        mainfactory()
         {
-            cout<<"1";
-            temp = rm->getmark(i);
-            cout<<i+1<<": "<<temp.name<<", "<<temp.country<< ", "<<temp.amount << ((temp.amount == "1") ? " piece. Made of " : " pcs. Made of ") << materials[temp.material-'0'-1]
-            <<", for "<<purposes[temp.purpose-'0'-1]<<"."<<endl<<"Emitent type: "<<emittypes[temp.emittype-'0'-1]<<". Shape: "<<shapes[temp.shape-'0'-1]<<". "
-            <<((temp.prestory == "---")?temp.prestory: "")<<endl<<"----------------------------------------------"<<endl;
-        }*/
-        rm->show(1);
-        vm->show(rm->getamt());
+            rm = new realmark;
+            vm = new virmark;
+        }
 
-    }
-
-    void newmark()
-    {
-        cout<< "Country: ";
-        scanf(" %[^\n]s",charbuf);
-        buf = new string(charbuf);
-        //string buf(k);
-        char isreal = '0';
-        for (int i = 0;i<195;i++)
-            if (*buf == countrieslist[i])
-            {
-                isreal = '1';
-                break;
-            }
-        if (isreal == '1') rm->setmark(*buf);
-        else vm->setmark(*buf);
-    }
-
-    void deletemark()
-    {
-        int select;
-        if (rm->getamt()>0 || vm->getamt()>0)
+        void show()
         {
-            cout<< "Put in number from 1 to " << rm->getamt()+vm->getamt() <<", 0 to abort: ";
-            cin>>select;
-            if (select!=0)
-            {
-                select--;
-                if (select<rm->getamt()&&select>0)
+            rm->show(1);
+            vm->show(rm->getamt()+1);
+        }
+
+        void newmark()
+        {
+            cout<< "Country: ";
+            scanf(" %[^\n]s",charbuf);
+            buf = string(charbuf);
+
+            char isreal = '0';
+            for (int i = 0;i<195;i++)
+                if (buf == countrieslist[i])
                 {
-                    rm->deletemark(select);
+                    isreal = '1';
+                    break;
                 }
-                else
+            if (isreal == '1') rm->setmark(buf);
+            else vm->setmark(buf);
+        }
+
+        void deletemark()
+        {
+            int select;
+            if (rm->getamt()>0 || vm->getamt()>0)
+            {
+                cout<< "Put in number from 1 to " << rm->getamt()+vm->getamt() <<", 0 to abort: ";
+                cin>>select;
+                if (select!=0)
                 {
-                    if (select-rm->getamt()<vm->getamt()&&select>0)
+                    select--;
+                    if (select<rm->getamt()&&select>=0&&rm->getamt()>0)
                     {
-                        rm->deletemark(select-rm->getamt());
+                        rm->deletemark(select);
                     }
-                    else cout<<"Error! No element with number "<<select+1<<"!"<<endl;
-
+                    else
+                    {
+                        if (select-rm->getamt()<vm->getamt()&&select>=0&&vm->getamt())
+                        {
+                            vm->deletemark(select-rm->getamt());
+                        }
+                        else
+                        {
+                            cout<<"Error! No element with number "<<select+1<<"!"<<endl;
+                        }
+                    }
                 }
+            }
+            else cout<<"No marks are in the list!"<<endl;
+        }
+
+        void save()
+        {
+            ofstream fin;
+            fin.open("save.sav",std::ios::out);
+            fin<<rm->getamt()<<"\t"<<vm->getamt()<<endl;
+            for (int i = 0; i < rm->getamt();++i)
+            {
+                mark temp = rm->getmark(i);
+                fin<<temp.country<<"\t"<<temp.emittype<<"\t"<<temp.material<<"\t"<<temp.name<<"\t"<<temp.prestory<<"\t"<<temp.purpose<<"\t"<<temp.shape<<endl;
+            }
+            for (int i = 0; i < vm->getamt();++i)
+            {
+                mark temp = vm->getmark(i);
+                fin<<temp.country<<"\t"<<temp.emittype<<"\t"<<temp.material<<"\t"<<temp.name<<"\t"<<temp.prestory<<"\t"<<temp.purpose<<"\t"<<temp.shape<<endl;
+            }
+            fin.close();
+        }
+        void load()
+        {
+            vm->deleteall();
+            rm->deleteall();
+            int amt1, amt2;
+            string t;
+            mark temp;
+            ifstream fout;
+            try
+            {
+                fout.open("save.sav",std::ios::out);
+                getline(fout,t,'\t');
+                amt1 = stoi(t);
+                getline(fout,t);
+                amt2 = stoi(t);
+                for (int i = 0; i < amt1; i++)
+                {
+                    getline(fout,t,'\t'); temp.country = t;
+                    getline(fout,t,'\t'); temp.emittype = t[0];
+                    getline(fout,t,'\t'); temp.material = t[0];
+                    getline(fout,t,'\t'); temp.name = t;
+                    getline(fout,t,'\t'); temp.prestory = t;
+                    getline(fout,t,'\t'); temp.purpose = t[0];
+                    getline(fout,t); temp.shape = t[0];
+                    rm->getfromfile(temp);
+                }
+                for (int i = 0; i < amt2; i++)
+                {
+                    getline(fout,t,'\t'); temp.country = t;
+                    getline(fout,t,'\t'); temp.emittype = t[0];
+                    getline(fout,t,'\t'); temp.material = t[0];
+                    getline(fout,t,'\t'); temp.name = t;
+                    getline(fout,t,'\t'); temp.prestory = t;
+                    getline(fout,t,'\t'); temp.purpose = t[0];
+                    getline(fout,t); temp.shape = t[0];
+                    vm->getfromfile(temp);
+                }
+                fout.close();
+            }
+            catch(int error)
+            {
+                cout<<"Error! File is corrupt or non-existent!"<<endl;
             }
         }
-    }
-
-    void save()
-    {
-
-    }
-    void load()
-    {
-
-    }
 };
 
 int main()
@@ -262,7 +383,7 @@ int main()
         getline(fst,countrieslist[i]);
     do{
         cout << "1. New mark" << endl << "2. Delete mark" << endl << "3. Load from file" << endl << "4. Save in file" << endl << "5. View marks" << endl
-        << "6. Real countries list" << endl << "0. Quit" << endl;
+        << "6. Real countries list" << endl <<"7. Clear screen" << endl << "0. Quit" << endl;
         cin >> choice;
         cout << "=============================================="<<endl;
         switch(choice)
@@ -289,7 +410,11 @@ int main()
         }break;
         case 6:
         {
-            for (int i = 0; i <195; i++) cout<<i+1<<". "<<countrieslist[i]<<endl;
+            for (int i = 0; i <195; i++) cout<<i+1<<".\t"<<countrieslist[i]<<endl;
+        }break;
+        case 7:
+        {
+            system("cls");
         }break;
         }
         if (choice!=0) cout << "=============================================="<<endl;
